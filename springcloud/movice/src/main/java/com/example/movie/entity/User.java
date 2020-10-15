57d83428ba0138dbc3ac517f8d860a1919385bc7
yuexingzhong <yuexingzhong@thunisoft.com>
2020-10-15 20:02:21 +0800

    添加消费者

---
+++ springcloud/movice/src/main/java/com/example/movie/entity/User.java
@@ -0,0 +1,64 @@
+package com.example.movie.entity;
+
+import java.math.BigDecimal;
+
+/**
+ * @author YUEXINGZHONG  2020/10/14 17:05
+ * @version V1.0.0
+ */
+public class User {
+
+
+    private Long id;
+
+
+    private String username;
+
+
+    private String name;
+
+
+    private Integer age;
+
+    private BigDecimal balance;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
