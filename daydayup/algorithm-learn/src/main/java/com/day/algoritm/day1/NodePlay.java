d13d59855dbdc454f68e9440ee42ac50fa1ec604
yuexingzhong <yuexingzhong@jd.com>
2023-06-15 17:15:52 +0800

    添加链表结构

---
+++ daydayup/algorithm-learn/src/main/java/com/day/algoritm/day1/NodePlay.java
@@ -0,0 +1,25 @@
+package com.day.algoritm.day1;
+
+/**
+ * 描述:
+ * User: YueXZ
+ * DateTime: 2023/6/15 17:12
+ * Version：1.0.0
+ */
+public class NodePlay {
+}
+
+
+class ListNode {
+    int value;
+    ListNode next;
+
+    public ListNode(int value) {
+        this.value = value;
+    }
+
+    public ListNode(int value, ListNode next) {
+        this.value = value;
+        this.next = next;
+    }
+}
