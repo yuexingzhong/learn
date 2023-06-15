43350826f74be818b51937595ed215f85234097d
yuexingzhong <yuexingzhong@jd.com>
2023-06-15 17:07:07 +0800

    添加忽略文件

--- daydayup/thead-learn/src/main/java/com/day/thread/day1/ThreadAttribute.java
+++ daydayup/thead-learn/src/main/java/com/day/thread/day1/ThreadAttribute.java
@@ -9,6 +9,33 @@ package com.day.thread.day1;
 public class ThreadAttribute {
 
 
+    private static int a = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                a++;
+            }
+        });
+
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                a++;
+            }
+        });
+
+        thread1.start();
+
+        thread2.start();
+
+        thread2.join();
+        thread1.join();
+
+        System.out.println(a);
+    }
 
 
 }
