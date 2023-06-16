2612a7b13a27da84cab664687c932a7b719a138c
yuexingzhong <yuexingzhong@jd.com>
2023-06-16 14:32:21 +0800

    等cas

---
+++ daydayup/thead-learn/src/main/java/com/day/thread/day1/CasThread.java
@@ -0,0 +1,24 @@
+package com.day.thread.day1;
+
+/**
+ * 描述:
+ * User: YueXZ
+ * DateTime: 2023/6/16 14:30
+ * Version：1.0.0
+ */
+public class CasThread {
+
+
+    public static void main(String[] args) {
+        ThreadLocal<String> a = new ThreadLocal<>();
+        a.set("yxz");
+
+
+        Thread thread = new Thread(() -> {
+            System.out.println("线程隔离：" + a.get());
+        });
+        thread.start();
+
+        System.out.println(a.get());
+    }
+}
