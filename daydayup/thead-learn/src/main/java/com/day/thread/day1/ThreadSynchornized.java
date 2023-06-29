591f57288dd9d28d3d86258a93076fd4cb4762fa
yuexingzhong <yuexingzhong@jd.com>
2023-06-29 10:08:49 +0800

    aqs实现锁机制

--- daydayup/thead-learn/src/main/java/com/day/thread/day1/ThreadSynchornized.java
+++ daydayup/thead-learn/src/main/java/com/day/thread/day1/ThreadSynchornized.java
@@ -4,10 +4,12 @@ package com.day.thread.day1;
 public class ThreadSynchornized {
 
 
-    public  static void main(String[] args) {
+    public static void main(String[] args) {
         Object a = new Object();
         synchronized (a) {
 
         }
+
+
     }
 }
