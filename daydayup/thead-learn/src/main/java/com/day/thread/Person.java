87321d3ce95bf9a1d0ac4f2a74cadfbaabfc4fcb
yuexingzhong <yuexingzhong@jd.com>
2023-07-06 10:26:53 +0800

    添加阻塞队列 arrayBlockedQueue,LinkedBlockQueue

---
+++ daydayup/thead-learn/src/main/java/com/day/thread/Person.java
@@ -0,0 +1,46 @@
+package com.day.thread;
+
+/**
+ * desc:
+ * User: YueXZ
+ * DateTime: 2023/7/5 14:36
+ * Version：1.0.0
+ */
+public class Person {
+
+    private String name;
+
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
--- daydayup/thead-learn/src/main/java/com/day/thread/QueueLearn.java
+++ daydayup/thead-learn/src/main/java/com/day/thread/QueueLearn.java
@@ -1,7 +1,8 @@
 package com.day.thread;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.Queue;
-import java.util.concurrent.LinkedBlockingDeque;
 
 /**
  * desc:
@@ -24,15 +25,49 @@ public class QueueLearn {
 //
 //        queue.put("4");
 
-        Queue queue = new LinkedBlockingDeque<String>();
-        queue.add("1");
-        queue.add("2");
-        queue.add("3");
-        queue.add("4");
+//        Queue queue = new LinkedBlockingDeque<String>();
+//        queue.add("1");
+//        queue.add("2");
+//        queue.add("3");
+//        queue.add("4");
+//
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(queue.poll());
+//        }
+        Queue a = new PriorityQueue<String>();
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println(queue.poll());
-        }
+        a.add("1");
+        a.add("2");
+        a.add("5");
+        a.add("3");
+
+
+        System.out.println(a.poll());
+        System.out.println(a.poll());
+        System.out.println(a.poll());
+        System.out.println(a.poll());
+
+
+        Queue a1 = new PriorityQueue<Person>(10, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.getAge().compareTo(o2.getAge());
+            }
+        });
+
+        a1.add(new Person("张11", 1));
+        a1.add(new Person("张22", 2));
+        a1.add(new Person("张55", 5));
+        a1.add(new Person("张33", 3));
+        a1.add(new Person("张44", 4));
+
+        System.out.println(a1.poll().toString());
+        System.out.println(a1.poll().toString());
+        System.out.println(a1.poll().toString());
+        System.out.println(a1.poll().toString());
+        System.out.println(a1.poll().toString());
+        System.out.println(a1.poll().toString());
+        System.out.println(a1.poll().toString());
 
     }
 }
