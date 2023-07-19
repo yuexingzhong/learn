5b02c1cfc783d4ccd8947f1204a7943fb1e6551b
yuexingzhong <yuexingzhong@jd.com>
2023-07-19 14:19:12 +0800

    添加concureentHashMap

---
+++ daydayup/thead-learn/src/main/java/com/day/thread/ConcureentHashMapLearn.java
@@ -0,0 +1,17 @@
+package com.day.thread;
+
+/**
+ * desc:
+ * User: YueXZ
+ * DateTime: 2023/7/17 17:09
+ * Version：1.0.0
+ */
+public class ConcureentHashMapLearn {
+    static final int HASH_BITS = 0x7fffffff; // usable bits of normal node hash
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
